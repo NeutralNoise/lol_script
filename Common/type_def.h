9db4d9c0eab1901e31555994d74f87634405a3ba
NeutralNoise <classicbrad24@gmail.com>
2020-08-13 02:59:38 +1000

    Added 16 bit typedefs.

--- Common/type_def.h
+++ Common/type_def.h
@@ -5,6 +5,8 @@ typedef unsigned long long uint64;
 typedef long long int64;
 typedef int int32;
 typedef unsigned int uint32;
+typedef short int16;
+typedef unsigned short uint16;
 
 
 #endif //TYPE_DEF_H
