632b6a56c9caa3d44e93e2006cedec5d6730e3e2
NeutralNoise <classicbrad24@gmail.com>
2020-08-18 01:54:05 +1000

    Added int8 and moved around int16 to make more sense.

--- Common/type_def.h
+++ Common/type_def.h
@@ -1,12 +1,14 @@
 #ifndef TYPE_DEF_H
 #define TYPE_DEF_H
 
+typedef char int8;
+typedef unsigned char int8;
+typedef short int16;
+typedef unsigned short uint16;
 typedef unsigned long long uint64;
 typedef long long int64;
 typedef int int32;
 typedef unsigned int uint32;
-typedef short int16;
-typedef unsigned short uint16;
 
 
 #endif //TYPE_DEF_H
